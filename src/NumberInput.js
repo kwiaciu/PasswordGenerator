b753180d9d5e52ca21fc76df724a5f172f86b0c6
kwiaciu <bkwiatek3@gmail.com>
2020-05-26 22:54:32 +0200

    ccchanges

--- src/NumberInput.js
+++ src/NumberInput.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import './NumberInput.css';
 
 export class NumberInput extends React.Component {
-  
+
   handleUserInput = (e) => {
     this.props.onUserInputChange(e.target.value, e.target.name);
   }
 
   render() {
     return (
-        <input name={this.props.name} onChange={this.handleUserInput} min='2' max='50' type="range" defaultValue='12' />
+      <input name={this.props.name} onChange={this.handleUserInput} min='2' max='50' type="range" defaultValue='12' />
     );
   }
 }
